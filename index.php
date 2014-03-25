81faf7b49c6225eade4d51f1dfeaa62aecc0b637
nathan <nathan.prior.31@gmail.com>
2014-03-25 18:00:01 +0100

    API REST velo

--- index.php
+++ index.php
@@ -6,21 +6,41 @@
 	<title>UPS TRANSPORT</title>
 	<link rel="stylesheet" type="text/css" href="css/semantic.css">
 	<link rel="stylesheet" type="text/css" href="css/index.css">
-  	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js"></script>
+        <script src="javascript/semantic.js"></script>
+  	
 </head>
 <body>
+    <?php 	
+		//RECUPERATION FORMAT JSON DE TOUTES LES LIGNES TISSEO
+		$lines = file_get_contents('http://pt.data.tisseo.fr/linesList?format=json&key=a03561f2fd10641d96fb8188d209414d8');
+		//print_r($lines);
+                
+                //cle JCDECEAUX
+                //1ef4a16b7ad8c600c6e505f8a5d1167fe873de42
+                
+                $velo = file_get_contents('https://api.jcdecaux.com/vls/v1/stations/100?contract=Toulouse&apiKey=1ef4a16b7ad8c600c6e505f8a5d1167fe873de42');
+                $parsed_json = json_decode($velo);
+                $nbBorneTotal = $parsed_json->{'bike_stands'};
+                $nbBorneDispo = $parsed_json->{'available_bike_stands'};
+                $ouvert = $parsed_json->{'status'};
+                $adresse = $parsed_json->{'address'};
+                $nbVeloDispo = $parsed_json->{'available_bikes'}; 
+	 ?>
+
 	
 	<header id="header" class="test">
-		<h1 class="title">UPS TRANSPORT</h1>
+            <div class="logo"><img  src="images/logo.png"></div>
+                   
 	</header><!-- /header -->
 		
 	<div id="corps">
 		<div class="ui two column middle aligned relaxed grid basic segment">
     <div id="actu" class="center aligned column" >
-      <h3>TRANSPORTS EN COMMUN DISPONIBLES</h3><br>
-      <div class="ui animated list" style="width:80%;">
-      <div class="item">
-        <img class="ui avatar image" src="images/bus.png">
+      <h2 class="section">TRANSPORTS EN COMMUN DISPONIBLES</h2><br>
+      <div class="ui animated list" style="width:90%;">
+      <div class="item ui piled segment">
+        <img class="ui avatar image transport_lis_icon" src="images/bus.png">
         <div class="content">
           <div class="header">Ligne 34</div>
           Bus en direction de ...    <br>Arrivée dans 15min
@@ -29,8 +49,8 @@
           <i class="thumbs up icon"></i>
         </div>
       </div>
-      <div class="item">
-        <img class="ui avatar image" src="images/bus.png">
+      <div class="item ui piled segment">
+        <img class="ui avatar image transport_lis_icon" src="images/bus.png">
         <div class="content">
           <div class="header">Ligne 38</div>
           Bus en direction de ... <br>Arrivée dans 5min
@@ -39,8 +59,8 @@
           <i class="thumbs up icon"></i>
         </div>
       </div>
-      <div class="item">
-        <img class="ui avatar image" src="images/bus.png">
+      <div class="item ui piled segment">
+        <img class="ui avatar image transport_lis_icon" src="images/bus.png">
         <div class="content">
           <div class="header">Ligne 42</div>
           Bus en direction de ... <br>Arrivée dans 21min
@@ -49,8 +69,8 @@
           <i class="thumbs up icon"></i>
         </div>
       </div>
-      <div class="item">
-        <img class="ui avatar image" src="images/metro.png">
+      <div class="item ui piled segment">
+        <img class="ui avatar image transport_lis_icon" src="images/metro.png">
         <div class="content">
           <div class="header">Metro ligne B</div>
           Direction Ramonville <br>Arrivée dans 12min
@@ -59,8 +79,8 @@
           <i class="thumbs up icon"></i>
         </div>
       </div>
-      <div class="item">
-        <img class="ui avatar image" src="images/metro.png">
+      <div class="item ui piled segment">
+        <img class="ui avatar image transport_lis_icon" src="images/metro.png">
         <div class="content">
           <div class="header">Metro ligne B</div>
           Direction Borderouge <br>Arrivée dans 7min
@@ -69,11 +89,18 @@
           <i class="thumbs up icon"></i>
         </div>
       </div>
-      <div class="item">
-        <img class="ui avatar image" src="images/velo.png">
-        <div class="content">
-          <div class="header">VélôToulouse</div>
-          ....
+      <div class="item ui piled segment ">
+        <img class="ui avatar image transport_lis_icon" src="images/velo.png">
+        <div class="content"">
+             <div class="header">VélôToulouse</div>
+                <?php 
+                     echo "$adresse <br> Nombre de vélos disponibles : $nbVeloDispo" 
+                ?>
+            <div class="test more_infos">
+                 <?php 
+                    echo "Nombre de bornes : $nbBorneTotal <br> Nombre de bornes disponibles : $nbBorneDispo" 
+                 ?>   
+            </div>
         </div>
         <div class="right floated ui like corner label">
           <i class="thumbs up icon"></i>
@@ -81,15 +108,13 @@
       </div>
     </div>
     </div>
-
-
     <div class="ui vertical divider">
       OU
     </div>
 
 
       <div id="itineraire" class="aligned column">
-        <center><h3>RENTRER CHEZ MOI</h3></center><br>
+        <h2 class="section">RENTRER CHEZ MOI</h2><br>
       <div class="ui form segment">
         <div class="field">
           <h5>ADRESSE</h5>
@@ -113,15 +138,21 @@
 	</div>
 	
 	<footer>
-		<br><br><br><br><br><br><br><br><br><br><br><br>
+		<br><br>
 	</footer>
-	
+        
+        <script>
+      $( ".item" ).click(function() {
+          //remplacer par this, mais ne marche pas
+          $( ".test" ).toggleClass( "more_infos_active" );
+      });
+      
+                                
+    </script>
+        
 
-	<?php 	
-		//RECUPERATION FORMAT JSON DE TOUTES LES LIGNES TISSEO
-		$lines = file_get_contents('http://pt.data.tisseo.fr/linesList?format=json&key=a03561f2fd10641d96fb8188d209414d8');
-		print_r($lines);
-	 ?>
-
+	<?php 
+        var_dump($velo);
+        ?>
 </body>
 </html>
