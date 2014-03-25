2761c45ef89d3b3f6d16b2097357225b24e48e43
nathan <nathan.prior.31@gmail.com>
2014-03-25 18:09:10 +0100

    test commit

---
+++ controller/Database.php
@@ -0,0 +1,45 @@
+<?php
+
+class Database {
+    private static $bdd = NULL;
+    /**
+     * Réalise la connexion à la base de donnée
+     * @return \PDO Une variable de connexion à la base de donnée
+     */
+    private static function connect(){ 
+        try{ 
+            return new PDO('mysql:host=localhost;dbname=upsTransportDB', '', '');
+        } catch (Exception $e){ 
+            die('Erreur : ' . $e->getMessage()); 
+        } 
+    } 
+    
+    /**
+     * Donne la connexion à la base de donnée, et la crée si elle n'existe pas.
+     * @return \PDO Une variable de connexion à la base de donnée.
+     */
+    public static function getConnection(){
+        if(Database::$bdd == NULL){
+            Database::$bdd = Database::connect();
+        }
+        return Database::$bdd;
+    }
+    
+    public static function getOneData($req){
+        $bdd = Database::getConnection();
+        $reponse = $bdd->query($req);
+        $donnees = $reponse->fetch();
+        $reponse->closeCursor();
+        return $donnees;
+    }
+    
+    public static function getAllData($req){
+        $bdd = Database::getConnection();
+        $reponse = $bdd->query($req);
+        return $reponse;
+    }
+    
+    
+}
+
+?>
